2a13d71d6ac71d300be35f57c5c344ce86e914ab
Aria Sadeghi <ariasadeghi@gmail.com>
2020-01-30 22:51:26 -0800

    added logic to display events

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,19 @@
 
+//Set current date on page load
 let currentTime = moment().format("dddd, MMMM Do YYYY");
 console.log(currentTime)
-$('#header-date').text(currentTime);
+document.getElementById('header-date').textContent = currentTime;
+
+document.addEventListener('click', (event) => {
+  event.preventDefault();
+  if(event.target.classList.contains('uk-button')){
+  let theText = document.createElement('p');
+  theText.textContent = event.target.parentNode.parentNode.parentNode.childNodes[3].childNodes[1].childNodes[1].value;
+  console.log(document.getElementsByClassName('uk-input').value);
+  console.log(event.target.parentNode.parentNode.parentNode.childNodes[3].childNodes[1].childNodes[1]);
+  event.target.parentNode.parentNode.parentNode.childNodes[3].childNodes[1].prepend(theText);
+  event.target.parentNode.parentNode.parentNode.childNodes[3].childNodes[1].childNodes[1].value = '';
+  }
+})
+
 
