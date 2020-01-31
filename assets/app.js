8bee8a4efd5ae58e1658fff978c75cd5cb1f868c
Aria Sadeghi <ariasadeghi@gmail.com>
2020-01-30 23:04:44 -0800

    can add items to calendar

--- assets/app.js
+++ assets/app.js
@@ -4,16 +4,17 @@ let currentTime = moment().format("dddd, MMMM Do YYYY");
 console.log(currentTime)
 document.getElementById('header-date').textContent = currentTime;
 
+// 
 document.addEventListener('click', (event) => {
   event.preventDefault();
   if(event.target.classList.contains('uk-button')){
   let theText = document.createElement('p');
   theText.textContent = event.target.parentNode.parentNode.parentNode.childNodes[3].childNodes[1].childNodes[1].value;
-  console.log(document.getElementsByClassName('uk-input').value);
+    console.log(document.getElementsByClassName('uk-input uk-form-blank uk-padding').value);
   console.log(event.target.parentNode.parentNode.parentNode.childNodes[3].childNodes[1].childNodes[1]);
   event.target.parentNode.parentNode.parentNode.childNodes[3].childNodes[1].prepend(theText);
-  event.target.parentNode.parentNode.parentNode.childNodes[3].childNodes[1].childNodes[1].value = '';
-  }
+    document.getElementsByClassName('uk-input uk-form-blank uk-padding').value = '';
+}
 })
 
 
