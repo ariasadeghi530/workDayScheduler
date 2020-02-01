4b65aa547374ac493103cf0b283c23e582e4d9ee
Aria Sadeghi <ariasadeghi@gmail.com>
2020-01-31 16:08:38 -0800

    updated logic

--- assets/app.js
+++ assets/app.js
@@ -3,19 +3,24 @@
 let currentTime = moment().format("dddd, MMMM Do YYYY");
 console.log(currentTime)
 document.getElementById('header-date').textContent = currentTime;
+let timeNow = moment().format('H');
+console.log(timeNow);
+// add value to the div box element, iterate over all the div boxes every time interval, and if now is >, <, or = to value, change background color 
 
-// Event 
+console.log(document.getElementsByClassName('parent-elem')[0].childNodes[3].childNodes[1].childNodes[1]);
+
+// Save the typed content as the placeholder text
 document.addEventListener('click', (event) => {
   event.preventDefault();
+
   if(event.target.classList.contains('uk-button')){
-  let theText = document.createElement('p');
-  theText.classList ='uk-margin-left uk-margin-top';
   theText.textContent = event.target.parentNode.parentNode.parentNode.childNodes[3].childNodes[1].childNodes[1].value;
-  console.log(document.getElementsByClassName('uk-input uk-form-blank uk-padding').value);
-  console.log(event.target.parentNode.parentNode.parentNode.childNodes[3].childNodes[1].childNodes[1]);
-  event.target.parentNode.parentNode.parentNode.childNodes[3].childNodes[1].prepend(theText);
-  //document.getElementsByClassName('uk-input uk-form-blank uk-padding').value = '';
+  event.target.parentNode.parentNode.parentNode.childNodes[3].childNodes[1].placeholder = theText;
+  document.getSelection().removeAllRanges();
 }
+
 })
+
+
 
 
