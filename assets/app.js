b901c550a214e595fff96183266d38e85821e626
Aria Sadeghi <ariasadeghi@gmail.com>
2020-01-31 20:44:32 -0800

    updated logic with local storage

--- assets/app.js
+++ assets/app.js
@@ -3,22 +3,34 @@
 let currentTime = moment().format("dddd, MMMM Do YYYY");
 document.getElementById('header-date').textContent = currentTime;
 
-
+//keep in military time for simplicity of logic
 let timeNow = moment().format('H');
 
 //compare time now to calendar hours, and change text area color depending on time
 for (let i = 9; i <= 17; i++) {
+  let textInd = i + 10;
 
   if (parseInt(document.getElementById(`${i}`).id) === timeNow) {
     document.getElementById(`${i}`).innerHTML = `
-      <input class="uk-input uk-textarea uk-padding uk-background-primary" type="text">
+      <input class="uk-input uk-textarea uk-padding uk-background-primary" id="${textInd}" type="text">
       `
   }
-  else if (parseInt(document.getElementById(`${i}`).id) < timeNow) {
+  else if (parseInt(document.getElementById(`${i}`).id) < timeNow)  {
     document.getElementById(`${i}`).innerHTML = `
-      <input class="uk-input uk-textarea uk-padding" type="text" disabled>
-      `
+      <input class="uk-input uk-textarea uk-padding" id="${textInd}" type="text" disabled>
+      `;
+      //at end of work day, clear all events
+    if (parseInt(timeNow) > 18){
+      localStorage.clear();
+      }
   }
+  //at end of work day, clear all events
+  
+   
+  
+
+  //display saved events from local storage, or just leave blank
+  document.getElementById(`${i}`).childNodes[1].value = localStorage.getItem(textInd) || '';
 }
 
 
@@ -27,9 +39,16 @@ document.addEventListener('click', (event) => {
   event.preventDefault();
 
   if (event.target.classList.contains('uk-button')) {
-    theText = event.target.parentNode.parentNode.parentNode.childNodes[3].childNodes[1].childNodes[1].value;
+
+    let theText = event.target.parentNode.parentNode.parentNode.childNodes[3].childNodes[1].childNodes[1].value;
     event.target.parentNode.parentNode.parentNode.childNodes[3].childNodes[1].placeholder = theText;
+
+    let index = event.target.parentNode.parentNode.parentNode.childNodes[3].childNodes[1].childNodes[1].id;
+    localStorage.setItem(index, theText);
+
+    //deslect text areas after clicking save button
     document.getSelection().removeAllRanges();
+
   }
 
 })
